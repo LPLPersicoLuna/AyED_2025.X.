d9744171fa3bdb9ee0a2b2851cef2acbca9b9e40
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-03-26 16:48:16 -0300

    Add files via upload

---
+++ cine ufff.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string pelicula;
+
+            Console.WriteLine("Hola usario");
+            Console.Write("¿Cula es tu nombre?: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("¿ Pelicula favorita?: ");
+            pelicula = Console.ReadLine();
+            Console.Write("que lindo nombre" + nombre);
+            Console.Write("tu pelicula favorita es una porqueria" + pelicula);
+
+
+
+
+
+
+        }
+    }
+}
