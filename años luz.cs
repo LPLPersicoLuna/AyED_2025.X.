6ba4b938178c6ad34b5ada7f1a920f508748b5ad
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-04-14 16:13:04 -0300

    Add files via upload

---
+++ años luz.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Ingresa distancia del curzo");
+            int añosluz = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa la  conbustible disponible");
+            int cd = int.Parse(Console.ReadLine());
+
+            if (añosluz * 12 <= cd)
+            {
+                Console.WriteLine("calculos esxactos, listos para el salto");
+
+            }
+            else
+            {
+                int faltante = (añosluz * 12) - cd;
+                Console.WriteLine("calculos inexactos, no se podra dar elsalto luz" + faltante + "litros");
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
