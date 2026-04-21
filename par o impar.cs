a8ab36ba601d0a7f5528375b55a465c5e6262bd0
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-04-21 16:21:28 -0300

    Add files via upload

---
+++ par o impar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //NUMERO
+            Console.WriteLine(" Querido usario ingrese numero ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            
+            if (  numero%2 == 0 )
+
+                Console.WriteLine(" Es par  ");
+
+            else
+                Console.WriteLine(" Error: Es impar  ");
+
+
+
+        }
+    }
+}
