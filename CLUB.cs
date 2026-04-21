ce62095452bcf6e2e507b80b9091d5fb782df088
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-04-21 15:55:37 -0300

    Add files via upload

---
+++ CLUB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            
+            // EDAD Y NOMBRE
+            Console.WriteLine("Ingresa nombre");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingresa edada");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            //PASA O  NO PASA
+            if ( edad > 18 )
+                Console.WriteLine("Bienvenido al club" +   nombre  );
+
+            else
+                Console.WriteLine("Lo siento, eres muy polluelo para esto");
+ 
+
+        }
+    }
+}
