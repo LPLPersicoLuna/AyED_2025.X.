2453d3e683e80742ed3c2ba06c15fd664446f3a5
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-04-21 15:27:52 -0300

    Add files via upload

---
+++ hoo más depacio velocista.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // VELOCIDAD
+            Console.WriteLine("Ingresa velocidad");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            //MULTA
+            if ( a > 120 )
+                Console.WriteLine( "Multa en camino: HOO más despacio velocista" );
+
+            else
+                Console.WriteLine("Conducción segura");
+        }
+    }
+}
