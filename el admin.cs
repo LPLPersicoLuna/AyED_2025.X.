3449ab5aa1818c671b688fca2e41feb7d48ea1e4
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-04-30 15:31:29 -0300

    Add files via upload

---
+++ el admin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string admin;
+
+            Console.WriteLine(" hola querido usario ");
+            Console.WriteLine("Ingrese nombre ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese contraseña ");
+            int contraseña = Convert.ToInt32(Console.ReadLine());
+            
+
+            if ("nombre" == "admin") 
+            {
+
+
+                Console.WriteLine(" Hola" + nombre + "La contraseña es" + 1234 + "PUEDE INGREAS" );
+
+            }
+            else
+            {
+                Console.WriteLine("Estas haciendo pasar por el admin  que mal" );
+
+
+            }
+
+
+        }
+    }
+}
