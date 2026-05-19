bc3a40941d18674953552d45548e91066498cd18
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-05-19 17:02:16 -0300

    Add files via upload

---
+++ sin termeninar ingresos cuales si solo tengo 100$.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese sus ingreso");
+            int ingresos = Convert.ToInt32(Console.ReadLine());
+         
+                Console.WriteLine("ingreso:" + ingresos);
+                ingresos++;
+            while (ingresos > 0)
+
+
+
+
+
+
+            }
+    }
+}
