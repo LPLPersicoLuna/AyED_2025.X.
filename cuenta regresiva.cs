30f4298311ba36107d7889ac470dc48db971bc70
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-05-19 15:22:10 -0300

    Add files via upload

---
+++ cuenta regresiva.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" Ingrese numero entero y positivo porfavor");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            while (numero > 0)
+            {
+                Console.WriteLine("numero:" + numero);
+                numero--;
+            }
+
+            
+
+        }
+    }
+}
