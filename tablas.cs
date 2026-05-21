514df8c7bf993c51869fa1ac1aa55bd12f382e39
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-05-21 15:47:08 -0300

    Add files via upload

---
+++ tablas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Ingresa  tabla porfavor");
+            int numero = int.Parse(Console.ReadLine());
+            int contador = 0;
+            int  multiplicacion = 0;
+            while (contador <=12)
+            {
+                multiplicacion = numero * contador;
+                Console.WriteLine(" la tabla es: "  + contador + "=" + multiplicacion);
+                contador++;
+            }
+
+
+        }
+    }
+}
