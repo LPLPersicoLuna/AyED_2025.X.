c4d49af88255974be4e11fb15355edae9d58ddad
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-05-21 16:47:36 -0300

    Add files via upload

---
+++ Promedio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota, contador, Promedio;
+           
+             Promedio= 0 ;
+             contador = 0;
+            do
+            {
+                Console.Write("nota");
+                nota = int.Parse(Console.ReadLine());
+                Promedio += nota;
+                contador += 1;
+            } while (contador < 5);
+            int promedioReal;
+            promedioReal = Promedio / 5;
+            Console.WriteLine("Promedio : " + Promedio + promedioReal );
+
+
+
+
+
+
+        }
+    }
+}
