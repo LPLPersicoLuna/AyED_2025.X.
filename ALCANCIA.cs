2b77600e51e22e355c9b64e44d1da3c8685d09c9
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-05-21 14:44:21 -0300

    Add files via upload

---
+++ ALCANCIA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Al cansia");
+            int Alcansia = 0;
+            int ingresos;
+
+            do
+            {
+                Console.WriteLine("ingrese sus ingresos");
+                ingresos = Convert.ToInt32(Console.ReadLine());
+
+                
+                Alcansia = Alcansia + ingresos;
+
+            } while (ingresos != 0);
+            Console.WriteLine("Tus ingresos totales" + Alcansia);
+
+
+
+        }
+    }
+}
