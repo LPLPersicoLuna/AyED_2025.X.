56d7100cef895f1b04a1740e4eec489d00b1e317
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-05-21 16:22:24 -0300

    Add files via upload

---
+++ contraseña.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Igrese contraseña : ");
+            string Contraseña = (Console.ReadLine());
+            string Contraseñaoficial;
+
+            do
+            {
+                Console.Write(" revise la Contraseña : ");
+                Contraseñaoficial = (Console.ReadLine());
+                if (Contraseña != Contraseñaoficial)
+                {
+                    Console.Write("  ERROR DE CONTRASEÑA : ");
+                }
+            } while (Contraseña != Contraseñaoficial);
+
+            Console.Write("  Contraseña validad prosigue : ");
+
+        }
+    }
+}
