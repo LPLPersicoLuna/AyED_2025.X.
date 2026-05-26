d7e691ef4f290fcd1b1f5df64068acff75081908
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-05-26 14:34:07 -0300

    Add files via upload

---
+++ La temperatura es de.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" temperatura: ");
+
+
+
+            Console.Write(" Ingresa la temperatura porfavor");
+             int temperatura = int.Parse(Console.ReadLine());
+            
+            while (temperatura > 24)
+            {
+                temperatura--;
+                Console.WriteLine("la temperatura: " + temperatura);
+
+
+            }
+
+            Console.Write(" La temperatura es: ");
+
+
+
+
+
+
+
+        }
+    }
+}
