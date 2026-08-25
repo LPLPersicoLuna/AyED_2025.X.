497a1f2271bba110aec0673b55f60a83df27b0c0
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-08-25 16:42:17 -0300

    Add files via upload

---
+++ cuenta de en teros pa tras.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            ContarRegresivo();
+        }
+
+        static void ContarRegresivo()
+
+        {
+
+            int contador = 1;
+            
+            int numero = 0;
+
+            Console.WriteLine("Ingrese numero entero:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            while (numero > 0)
+            {
+                numero = numero - 1;
+                Console.WriteLine(numero);
+               contador++;
+            }
+
+
+
+
+
+        }
+    }
+}
