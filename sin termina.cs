b992749303bf25cb0f2f01212835cf28fab24ddf
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-09-01 17:01:54 -0300

    Add files via upload

---
+++ sin termina.cs
@@ -0,0 +1,53 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            enteros();
+        }
+
+
+        static void enteros()
+        {
+            int contador = 0; 
+            int numero = 0;
+            int par = 0;
+            Console.WriteLine("Ingrese numero entero: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            while (numero < 20)
+            {
+                numero = numero + 1;
+                Console.WriteLine(numero);
+
+             
+                if (numero > 10)
+                {
+                    contador++;
+                }
+            }
+            
+            
+
+            if (2%==000
+                )
+          
+
+
+
+
+
+
+
+        }
+        }
+    }
