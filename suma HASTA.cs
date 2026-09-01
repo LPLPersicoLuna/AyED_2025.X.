7c9448c8148b81e7808ac4bc63a98e1da6685db8
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-09-01 14:14:06 -0300

    Add files via upload

---
+++ suma HASTA.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 5;
+            int resultado = SumarHasta(numero);
+
+            Console.WriteLine("La suma desde 1 hasta" +numero + "es:" + resultado);
+        }
+
+       
+        static int SumarHasta(int limite)
+        {
+           
+            return (limite * (limite + 1)) / 2;
+        }
+    }
+
+
+
+}
+        
+
+
+
+
+
+
+    
+    
