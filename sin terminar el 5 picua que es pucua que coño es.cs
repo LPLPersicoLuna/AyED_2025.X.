2779ed423607588cac43e4affcc49a061edc98ed
LPLPersicoLuna <lucapersicoluna@gmail.com>
2026-09-08 16:57:40 -0300

    Add files via upload

---
+++ sin terminar el 5 picua que es pucua que coño es.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            Console.WriteLine("Ingrese un numero entero porfavor no sea tonto:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            int digito = (numero);
+
+
+        }
+    }
+}
